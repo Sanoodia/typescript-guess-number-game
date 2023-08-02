c6bd3869d0a88b82e08a35529578ef356c6f31a1
Sanoodia <sanoodia.micromerger@gmail.com>
2023-08-02 17:07:57 +0500

    game created

---
+++ game.ts
@@ -0,0 +1,70 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+import * as emoji from "node-emoji";
+
+function* lives() {
+  let index: number = 5;
+  while (index > 0) {
+    console.log(emoji.emojify(index === 5 ? 
+        `Your Lives:${" :heart: ".repeat(index)}` 
+        : `Your Remaining Lives:${" :heart: ".repeat(index)}`));
+    yield index--;
+  }
+}
+
+const promptGuess = async (attempts: number) => {
+  const prompt: {
+    guess: string;
+  } = await inquirer.prompt([
+    {
+      name: "guess",
+      type: "input",
+      message: attempts < 5 ? "Guess Again " : "Guess Number Between 0-9:",
+      validate: (input: string) => {
+        const number = parseInt(input);
+        return (!isNaN(number))? true: "Input Should be a Number";
+      }
+    },
+  ]);
+  return parseInt(prompt.guess);
+};
+
+const startGame = () => {
+  console.clear();
+  let generateLives: any = lives();
+  let actualNumber: number = Math.floor(Math.random() * 10) + 1;
+
+  function continueGame() {
+    console.log(chalk.bgCyanBright.bold("\n        Guess Number         \n"));
+
+    let lives: number = generateLives.next().value;
+    if (lives) {
+      let guess = promptGuess(lives);
+      guess.then((guess) => {
+        if (guess === actualNumber) {
+          console.log(
+            chalk.bgGreen.bold(
+              "\uD83C\uDF89 \uD83C\uDF89 You won \uD83C\uDF89 \uD83C\uDF89"
+            )
+          );
+        } else {
+          console.clear();
+          continueGame();
+        }
+      });
+    } else {
+      console.log(
+        chalk.bgRed.bold(
+          "\n\uD83D\uDE22 \uD83D\uDE22 You lost \uD83D\uDE22 \uD83D\uDE22\n"
+        )
+      );
+      console.log(
+        chalk.gray.bold(`Actuall Number is  ${actualNumber}`)
+      );
+    }
+  }
+  return continueGame;
+};
+
+let initGame = startGame();
+initGame();
